2134d787cb53869f3fe89fd2a0001a57c3aaa1f4
Danish Sharma <adonis@Danishs-MacBook-Air.local>
2022-09-25 11:44:27 +0530

    Updates

---
+++ precache-manifest.efc1b3c55271a1b7036c03f73a684bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4a151dc997003575989",
+    "url": "/my-app/static/css/main.7f20667a.chunk.css"
+  },
+  {
+    "revision": "e4a151dc997003575989",
+    "url": "/my-app/static/js/main.e4a151dc.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/my-app/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "ed96fda1645a17fdc062",
+    "url": "/my-app/static/css/2.9da6b12e.chunk.css"
+  },
+  {
+    "revision": "ed96fda1645a17fdc062",
+    "url": "/my-app/static/js/2.ed96fda1.chunk.js"
+  },
+  {
+    "revision": "cf865ada5b79c739741c",
+    "url": "/my-app/static/js/runtime~main.cf865ada.js"
+  },
+  {
+    "revision": "a45df436d312324b41a28fec325769b4",
+    "url": "/my-app/index.html"
+  }
+];
